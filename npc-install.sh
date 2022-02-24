e143b61bb8e05ed05a478e1827e78e7278cd6ff9
ZhongMeng-wq <80760635+ZhongMeng-wq@users.noreply.github.com>
2022-02-25 00:08:30 +0800

    Update npc-install.sh

--- npc-install.sh
+++ npc-install.sh
@@ -10,30 +10,116 @@ if ! [ -x "$(command -v wget)" ]; then
     fi
 fi
 
-echo "欢迎来到npc客户端";
+function read_server()
+{
+    cat_npc_server=$(cat /npc/conf/npc.conf | grep "server_addr")
+    cat_npc_server_o="服务器地址与端口：${cat_npc_server/server_addr=/}"
+
+    cat_npc_key=$(cat /npc/conf/npc.conf | grep "vkey")
+    cat_npc_key_o="密钥：${cat_npc_key/vkey=/}"
+
+    cat_npc_type=$(cat /npc/conf/npc.conf | grep "conn_type")
+    cat_npc_type_o="协议：${cat_npc_type/conn_type=/}"
+
+    cat_npc_web_user=$(cat /npc/conf/npc.conf | grep "web_username")
+    cat_npc_web_user_o="客户端web用户名:${cat_npc_web_user/web_username=/}"
+
+    cat_npc_web_pass=$(cat /npc/conf/npc.conf | grep "web_password")
+    cat_npc_web_pass_o="客户端web密码:${cat_npc_web_pass/web_password=/}"
+
+    key_aaa_port=${cat_npc_key/vkey=/}
+}
+#读配置文件
+
+function server_config()
+{
+    read_server
+    #读取桥配置文件
+    echo "----------------------------------------"
+    echo "1.读取桥通信配置"
+    echo "2.修改桥通信配置"
+    echo "----------------------------------------"
+
+    read -rp "请输入选择:" -e server_number
+    echo "----------------------------------------"
+    if [[ $server_number = "1" ]]
+    then
+        read_server
+        echo $cat_npc_server_o
+        echo $cat_npc_key_o
+        echo $cat_npc_type_o
+        if [[ ${cat_npc_web_user/web_username=/} != "#" ]] 
+        then
+            echo $cat_npc_web_user_o
+        fi
+
+        if [[ ${cat_npc_web_pass/web_password=/} != "#" ]] 
+        then
+            echo $cat_npc_web_pass_o
+        fi
+        echo "----------------------------------------"
+    elif [[ $server_number = "2" ]]
+    then
+        read -rp "请输入服务器地址和端口号(例:127.0.0.1:80)：" -e server
+        read -rp "请输入服务器唯一密钥:" -e server_key
+        until [[ $npc_type =~ (tcp|kcp) ]]; do
+            read -rp "请选择服务器端通信传输协议类型(tcp|kcp): " -e npc_type
+        done
+
+        until [[ $web_aaa =~ (y|n) ]]; do
+            read -rp "是否需要配置web认证用户(y|n): " -e web_aaa
+        done
+        if [[ $web_aaa = "y" ]]
+        then
+            read -rp "请输入web端用户名:" -e web_user
+            read -rp "请输入web端密码:" -e web_pass
+        fi
+
+        echo "----------------------------------------"
+        echo "正在写入......"
+        read_server
+
+        sed -i "s/$cat_npc_server/server_addr=$server/g" `grep -rl "$cat_npc_server" /npc/conf/npc.conf`
+        sed -i "s/$cat_npc_key/vkey=$server_key/g" `grep -rl "$cat_npc_key" /npc/conf/npc.conf`
+        sed -i "s/$cat_npc_type/conn_type=$npc_type/g" `grep -rl "$cat_npc_type" /npc/conf/npc.conf`
+        if [[ $web_aaa = "y" ]]
+        then
+            sed -i "s/$cat_npc_web_user/web_username=$web_user/g" `grep -rl "$cat_npc_web_user" /npc/conf/npc.conf`
+            sed -i "s/$cat_npc_web_pass/web_password=$web_pass/g" `grep -rl "$cat_npc_web_pass" /npc/conf/npc.conf`
+        else
+            sed -i "s/$cat_npc_web_user/#web_username=/g" `grep -rl "$cat_npc_web_user" /npc/conf/npc.conf`
+            sed -i "s/$cat_npc_web_pass/#web_password=/g" `grep -rl "$cat_npc_web_pass" /npc/conf/npc.conf`      
+        fi
+        read_server
+        echo "完成"
+        echo "----------------------------------------"
+        echo $cat_npc_server_o
+        echo $cat_npc_key_o
+        echo $cat_npc_type_o
+        if [[ ${cat_npc_web_user/web_username=/} != "#" ]] 
+        then
+            echo $cat_npc_web_user_o
+        fi
+
+        if [[ ${cat_npc_web_pass/web_password=/} != "#" ]] 
+        then
+            echo $cat_npc_web_pass_o
+        fi
+        echo "--------------------------------------"
+    else
+        echo "输入有误"
+    fi
+
+}
+#写入桥配置
+
+echo "欢迎来到npc客户端2.0";
+echo "此次改为配置文件方式运行"
 echo "此客户端适用CentOS6/Debian8/Ubuntu16以上版本"
 echo "请选择类型:";
 echo "1:安装";
 echo "2:卸载";
-echo "3:修改配置";
-echo "4:退出";
-
-function npc_config()
-{
-    read -rp "请输入nps服务器IP地址或域名:" ipadd
-    read -rp "请输入nps服务器端口号:" port
-    read -rp "请输入唯一验证密钥:" vkey
-    until [[ $type =~ (tcp|kcp) ]]; do
-        read -rp "请选择传输类型(tcp|kcp): " -e type
-    done
-
-    chmod 777 /npc -R
-    /npc/npc install -server=$ipadd:$port -vkey=$vkey -type=$type
-    npc restart 
-    echo "-------------";
-    echo "配置完成";
-    echo "-------------";
-}
+echo "3:退出";
 
 until [[ $npc =~ (1|2|3|4) ]]; do
     read -rp "请选择要下载的版本: " -e npc
@@ -61,10 +147,10 @@ if [[ $npc = "1" ]]
                     rm -rf /root/linux_arm64_client*
                     rm -rf /root/linux_arm_v5_client*
                     rm -rf /usr/bin/npc*
+                    rm -rf /usr/bin/autonpc
                     /npc/npc stop
                     /npc/npc uninstall
                     rm -rf /npc
-                    mkdir /npc
 
             else
                 exit 1
@@ -136,8 +222,76 @@ if [[ $npc = "1" ]]
             fi
                 #国外
         # 判断选择类型
-        npc_config;
+        wget -P /usr/bin/ http://nps.loline.top/shell/fx/autonpc
 
+        rm -rf /npc/conf/npc.conf
+        touch /npc/conf/npc.conf
+        
+        echo "#--common--" >> /npc/conf/npc.conf
+        echo "[common]" >> /npc/conf/npc.conf
+        echo "server_addr=" >> /npc/conf/npc.conf
+        echo "conn_type=" >> /npc/conf/npc.conf
+        echo "vkey=" >> /npc/conf/npc.conf
+        echo "#web_username=" >> /npc/conf/npc.conf
+        echo "#web_password=" >> /npc/conf/npc.conf
+        echo "#--common--" >> /npc/conf/npc.conf
+
+        
+
+        read -rp "请输入服务器地址和端口号(例:127.0.0.1:80)：" -e server
+        read -rp "请输入服务器唯一密钥:" -e server_key
+        until [[ $npc_type =~ (tcp|kcp) ]]; do
+            read -rp "请选择服务器端通信传输协议类型(tcp|kcp): " -e npc_type
+        done
+
+        until [[ $web_aaa =~ (y|n) ]]; do
+            read -rp "是否需要配置web认证用户(y|n): " -e web_aaa
+        done
+        if [[ $web_aaa = "y" ]]
+        then
+            read -rp "请输入web端用户名:" -e web_user
+            read -rp "请输入web端密码:" -e web_pass
+        fi
+
+        echo "----------------------------------------"
+        echo "正在写入......"
+        read_server
+
+        sed -i "s/$cat_npc_server/server_addr=$server/g" `grep -rl "$cat_npc_server" /npc/conf/npc.conf`
+        sed -i "s/$cat_npc_key/vkey=$server_key/g" `grep -rl "$cat_npc_key" /npc/conf/npc.conf`
+        sed -i "s/$cat_npc_type/conn_type=$npc_type/g" `grep -rl "$cat_npc_type" /npc/conf/npc.conf`
+        if [[ $web_aaa = "y" ]]
+        then
+            sed -i "s/$cat_npc_web_user/web_username=$web_user/g" `grep -rl "$cat_npc_web_user" /npc/conf/npc.conf`
+            sed -i "s/$cat_npc_web_pass/web_password=$web_pass/g" `grep -rl "$cat_npc_web_pass" /npc/conf/npc.conf`
+        else
+            sed -i "s/$cat_npc_web_user/#web_username=/g" `grep -rl "$cat_npc_web_user" /npc/conf/npc.conf`
+            sed -i "s/$cat_npc_web_pass/#web_password=/g" `grep -rl "$cat_npc_web_pass" /npc/conf/npc.conf`      
+        fi
+        read_server
+        echo "完成"
+        echo "----------------------------------------"
+        echo $cat_npc_server_o
+        echo $cat_npc_key_o
+        echo $cat_npc_type_o
+        if [[ ${cat_npc_web_user/web_username=/} != "#" ]] 
+        then
+            echo $cat_npc_web_user_o
+        fi
+
+        if [[ ${cat_npc_web_pass/web_password=/} != "#" ]] 
+        then
+            echo $cat_npc_web_pass_o
+        fi
+        echo "--------------------------------------"
+
+
+
+        chmod 777 /usr/bin/autonpc
+        /npc/npc install -config=/npc/conf/npc.conf
+        npc restart -config=/npc/conf/npc.conf
+        echo "安装完成，请通过autonpc命令进入面板"
+        read -rp "输入任意键退出:" -e ry
 
         # npc配置文件
 elif [[ $npc = "2" ]]
@@ -152,6 +306,7 @@ elif [[ $npc = "2" ]]
                 rm -rf /root/linux_arm64_client*
                 rm -rf /root/linux_arm_v5_client*
                 rm -rf /usr/bin/npc*
+                rm -rf /usr/bin/autonpc
                 /npc/npc stop
                 /npc/npc uninstall
                 rm -rf /npc
@@ -169,16 +324,6 @@ elif [[ $npc = "2" ]]
 
 elif [[ $npc = "3" ]]
     then
-        if [ ! -d "/npc" ]; then
-            echo "未检测到 /npc 下安装 npc，请重新运行此脚本";
-            exit 1
-        else
-            npc_config;
-        fi
-        # 修改配置文件
-elif [[ $npc = "4" ]]
-    then
-        echo "Exit!"
         exit 1
 else
     echo "不符合条件，退出.";
